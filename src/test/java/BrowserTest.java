3898df6472b3e2901b7c8fbe19b04ad16a6fd45b
Sridhar <sridhrao@gmail.com>
2025-07-04 13:47:56 -0400

    testcommit

---
+++ src/test/java/BrowserTest.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserTest {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://www.facebook.com/");
+		 driver.manage().window().maximize();
+		 WebElement email = driver.findElement(By.xpath("//input[contains(@placeholder, 'Email')]"));
+		 email.sendKeys("sridhrao1@gmail.com");
+		 WebElement pwd = driver.findElement(By.xpath("//input[@id='pass']"));
+		 pwd.sendKeys("test");
+		 WebElement login = driver.findElement(By.xpath("//button[contains(text(),'Log In')]"));
+		 login.click();
+		 driver.get("https://www.facebook.com/");
+		 WebElement fpwd= driver.findElement(By.xpath("//a[contains(text(),'Forgot password?')]"));
+		 fpwd.click();
+		 WebElement email1= driver.findElement(By.xpath("//label//input[contains(@placeholder, 'Email')]"));
+		 email1.sendKeys("email@email.com");
+		 WebElement pwd2 = driver.findElement(By.xpath("//input[@name='pass']"));
+		 
+		 pwd2.sendKeys("test");
+		 //WebElement login1= driver.findElement(By.xpath("//span[contains(text(),'Log In')]]"));
+		 //login1.click();
+		 WebElement cancel = driver.findElement(By.xpath("//a[contains(text(),'Cancel')]"));
+		 cancel.click();
+		 driver.close();
+		
+		 
+		 }
+}
---
+++ src/test/java/pcori.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class pcori {
+	
+	public static void main(String args[]) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://pcori--pqt.sandbox.my.salesforce.com/");
+		driver.manage().window().maximize();
+		WebElement uname=driver.findElement(By.xpath("//input[@id='username']"));
+		
+		uname.sendKeys("sthota.ct@pcori.org");
+		WebElement pwd = driver.findElement(By.xpath("//input[@name='pw']"));
+		pwd.sendKeys("Prolifics@2025S");
+		driver.findElement(By.xpath("//input[@name='pw']")).click();
+		String title =driver.getTitle();
+		System.out.println("page title is " +title); /// test 
+		
+		driver.quit();
+		
+		
+	}
+
+}
