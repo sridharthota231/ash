6b8f8388bb46caf25de4788d4228dab7014f6ae3
Sridhar <sridhrao@gmail.com>
2025-07-05 02:34:40 -0400

    ok

--- src/test/java/pcori.java
+++ src/test/java/pcori.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class pcori {
 
     public static void main(String[] args) {
+    	
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+    	
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://pcori--pqt.sandbox.my.salesforce.com/");
